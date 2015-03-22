316d877fa4bfaa88a141aba0658f87e6ea43b133
jczetta <jczetta@gmail.com>
2015-03-22 14:43:46 -0400

    initial commit

---
+++ api/gwar.rb
@@ -0,0 +1,71 @@
+require 'grape'
+require 'warden'
+
+module GWAR
+  class API < Grape::API
+    format :json
+
+    SurvivalItem = Struct.new(:name, :weight, :value)
+
+    desc "get the proper survival pack"
+    #content-type "text/plain"
+    ## This takes care of parameter validation
+    params do
+      requires :maxwt, type: Integer
+    end
+    ## This takes care of creating pack
+    ## Have to write some more code here to handle knapsack
+    post do
+      #max = params[:maxwt]
+      {"answer" => params[:maxwt]} # this gets the right data from the curl request!
+    end
+    #end
+
+    # basic get works here
+    get "testroute" do
+      def self.return_thing()
+        {"answer" => "hahaha oh yeah"}
+      end
+      self.return_thing()
+    end
+
+
+
+    # authentication
+    get "warden" do
+      env['warden'].authenticate!
+      { "status_code" => 200, "status_text" => "Authorized"}
+    end
+
+    get "info" do
+        env['warden'].authenticate!
+        env['warden'].user.name
+    end
+
+    get 'unauthenticated' do
+        { "status_code" => 401, "status_text" => "Unauthorized"}
+    end
+
+    post 'login' do
+        env['warden'].authenticate(:password)
+    end
+  end
+
+  # borrowed class User
+  class User
+    attr_reader :name
+    attr_reader :id
+    def initialize(name)
+        @name = name
+        @id = 2015
+    end
+
+    def self.get(id)
+        return User.new('jackie')
+    end
+
+    def self.authenticate(u, p)
+        return User.new('jackie')
+    end
+  end
+end
---
+++ config.ru
@@ -0,0 +1,29 @@
+require File.join(File.dirname(__FILE__), 'api', 'gwar')
+
+if ENV['RACK_ENV'] == 'development'
+
+end
+
+Warden::Manager.serialize_into_session { |user| user.id }
+Warden::Manager.serialize_from_session { |id| GWAR::User.get(id) }
+
+use Rack::Session::Cookie, :secret => "abcdefg"
+
+use Warden::Manager do |manager|
+  manager.default_strategies :password
+  manager.failure_app = GWAR::API
+end
+
+Warden::Strategies.add(:password) do
+
+  def valid?
+    params['username'] || params['password']
+  end
+
+  def authenticate!
+    u = GWAR::User.authenticate(params['username'], params['password'])
+    u.nil? ? fail!("Could not log in") : success!(u)
+  end
+end
+
+run GWAR::API
