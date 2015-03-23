ad07da93b5b0d6f1932ef8a83614d9a1ba44c5a9
jczetta <jczetta@gmail.com>
2015-03-22 22:19:51 -0400

    fix bugs in convert tab data fxn

--- api/data/convert_tab_data.py
+++ api/data/convert_tab_data.py
@@ -3,17 +3,18 @@ import json
 
 # first command line arg: filename to open
 # second command line arg: filename to write (NOT the same)
-if sys[1] == sys[2]:
+if sys.argv[1] == sys.argv[2]:
 	print "Don't overwrite your file! Please run again."
-elif sys[2][-4:].upper() != "JSON":
+elif sys.argv[2][-4:].upper() != "JSON":
 	print "Please save input in a JSON file." # quick and easy conversion
 else:
-	f = open(sys[1],"r")
+	f = open(sys.argv[1],"r")
 	lines = f.readlines()
 	f.close()
 	tab_split = []
 	for l in lines:
-		sep = lines.split("\t")
+		sep = l.split("\t")
+		#print sep
 		tab_split.append(sep)
 
 	d = {}
@@ -21,9 +22,10 @@ else:
 	d["items"] = {}
 
 	for inp in tab_split[2:]:
+		print inp
 		d["items"][inp[0]] = {}
-		d["items"][inp[0]]["weight"] = inp[1]
-		d["items"][inp[0]]["value"] = inp[2]
+		d["items"][inp[0]]["weight"] = int(inp[1])
+		d["items"][inp[0]]["value"] = int(inp[2].strip())
 
-	jsonf = open(sys[2],"w")
-	jsonf.write(json.dumps(str(d)))
+	jsonf = open(sys.argv[2],"w")
+	jsonf.write(json.dumps(d))
